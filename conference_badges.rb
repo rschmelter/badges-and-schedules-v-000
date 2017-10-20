7d3168b6e68e3d33089578495d0aacf0db60f459
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-20 18:35:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,21 @@
-# Write your code here.
 def badge_maker(name)
 "Hello, my name is #{name}."
 end
 
-
+names_array = [
+"Edsger",
+"Ada",
+"Charles",
+"Alan",
+"Grace",
+"Linus",
+"Matz"  
+]
 
 def batch_badge_creator(names_array)
+  badge_messages = []
+  names_array.each do |names|
+  badge_messages << badge_maker(name)
+end
+  badge_messages
 end
