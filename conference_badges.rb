43be1a66ce012ffa16c25680d7ccb995de4aa871
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-20 19:25:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -30,7 +30,7 @@ def assign_rooms(names_array)
   room_assignments
 end
 
-def printer(batch_badge_creator, assign_rooms)
+def printer
   batch_badge_creator(names_array).each do |names|
     puts names
   end
