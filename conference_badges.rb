c8dc2c565c8835276cfc8736408a9816e7a98548
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-20 18:59:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,24 +14,27 @@ names_array = [
 
 def batch_badge_creator(names_array)
   badge_messages = []
-  names_array.each do |names|
+  names_array.each_with_index do |names|
   badge_messages << badge_maker(names)
 end
   badge_messages
 end
 
-rooms = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7"
-]
+# rooms = [
+#   "1",
+#   "2",
+#   "3",
+#   "4",
+#   "5",
+#   "6",
+#   "7"
+# ]
 
 def assign_rooms(speakers)
   room_assignments = []
-  names_array.each do |speaker|
-  room_assignments << "Hello #{speaker}! "
+  
+    names_array.each_with_index {|speaker, index|}
+    room_assignments << "Hello #{speaker}! You'll be assigned to room #{index + 1}!"
+  end
+  room_assignments
 end
